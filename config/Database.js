b3e67c9f652e9449caa89602d88fd4beef494710
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-07-08 11:40:30 +0700

    update db

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('butuhlaptop_db', 'root', 'root', {
+const db = new Sequelize('butuhlaptop_db', 'root', '', {
     host: "localhost",
     dialect: "mysql"
 });
