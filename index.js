28904cd2cde9c24bd1dadf60f527700f9300a3a6
AriqUntukmu <ariqfachry2611@gmail.com>
2023-10-25 22:19:47 +0700

    fix import

--- index.js
+++ index.js
@@ -6,20 +6,17 @@ import db from "./config/Database.js";
 import SequelizeStore from "connect-session-sequelize"
 
 //import model
-import User from "./models/UserModel.js";
-import Product from "./models/ProductModel.js";
-import Cart from "./models/CartModel.js";
-import Feedbacks from "./models/FeedbackModel.js";
-import Checkout from "./models/CheckoutModel.js";
-import CartItem from "./models/CartItemModel.js"
 
 //import route
 import UsersRoute from "./routes/UserRoutes.js"
 import ProductRoute from "./routes/ProductRoutes.js"
 import FeedbacksRoute from "./routes/FeedbackRoutes.js";
-import CheckoutRoute from "./routes/CheckoutRoutes.js"
-import CartRoute from "./routes/CartRoute.js"
-import CartItemRoute from "./routes/CartItemRoute.js"
+import User from "./models/User.js";
+import Product from "./models/Product.js";
+import Cart from "./models/Cart.js";
+import Feedbacks from "./models/Feedback.js";
+import OrderDetail from "./models/Order-Detail.js";
+import Order from "./models/Order.js";
 
 dotenv.config()
 
@@ -41,6 +38,14 @@ try {
 
     await db.sync();
     //opsi force: true bakal maksa sequelize utk menyamakan keseluruhan field db
+    await Promise.all([
+        User.sync(),
+        Feedbacks.sync(),
+        Product.sync(),
+        Cart.sync(),
+        OrderDetail.sync(),
+        Order.sync(),
+    ])
     
     //awal2 tetap harus ditulis satu per satu
     // await User.sync();
@@ -80,10 +85,7 @@ app.use(express.json())
 //middleware
 app.use(UsersRoute)
 app.use(ProductRoute)
-app.use(CartRoute)
-app.use(CartItemRoute)
 app.use(FeedbacksRoute)
-app.use(CheckoutRoute)
 
 store.sync()
 
