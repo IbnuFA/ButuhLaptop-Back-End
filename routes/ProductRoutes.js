cc3b91a50978f685ecfcb36c64bb6517546965c5
AriqUntukmu <ariqfachry2611@gmail.com>
2023-10-25 23:01:34 +0700

    disable add, edit, delete for user product route

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -12,14 +12,16 @@ import { verifyUser } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
+router.use(verifyUser)
+
 router.get('/products/test', (req, res) => {
   res.send("Test Router Products")
 }) 
 
 router.get('/products' ,getProducts);
 router.get('/products/:id', getProductsbyId);
-router.post('/products', createProducts);
-router.patch('/products/:id', updateProducts);
-router.delete('/products/:id', deleteProducts);
+// router.post('/products', createProducts);
+// router.patch('/products/:id', updateProducts);
+// router.delete('/products/:id', deleteProducts);
 
 export default router
