28452ebef00981b4ef4a7d4a62d9d1c86a4dc1ab
AriqUntukmu <ariqfachry2611@gmail.com>
2023-10-25 22:19:24 +0700

    config db using env

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize";
+import 'dotenv/config'
 
-const db = new Sequelize('butuhlaptop_db', 'root', '', {
-    host: "localhost",
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: "mysql"
 });
 
