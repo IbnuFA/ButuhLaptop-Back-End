70b1c21979bca6cc1de906927ca3defbd223b4e1
Yusril Adriansyah P <yusriladr.37@gmail.com>
2024-01-07 15:18:45 +0700

    add jsonwebtoken package

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,5 +1,6 @@
 import argon2 from "argon2"
 import User from "../models/User.js"
+import jwt from 'jsonwebtoken';
 import axios from "axios"
 import { getCityById, getProvinceById } from "../services/logistic-service.js"
 
